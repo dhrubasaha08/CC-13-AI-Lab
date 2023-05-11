f779de57242b0c20063757e127d5ecbb0ba6ae0b
Dhruba Saha <dhrubasaha08@gmail.com>
2023-05-11 19:55:49 +0530

    fix

--- Practical/add-list-all-one-list.pl
+++ Practical/add-list-all-one-list.pl
@@ -1,4 +1,5 @@
-%insert x into a list in all possible positions in one list.
+#insert x into a list in all possible positions in one list.
 
 insert(X, [], [X]).
-insert(X, [Y | T], [X, Y | T1]) :- insert(X, T, T1).
+insert(X, [Y|T], [X,Y|T]).
+insert(X, [Y|T], [Y|Z]) :- insert(X, T, Z).
