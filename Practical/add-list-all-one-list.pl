1aeeebe6ddc446cd2d9573ee830dbcf2ff1289b2
Dhruba Saha <dhrubasaha08@gmail.com>
2023-05-11 20:03:47 +0530

    del

--- Practical/add-list-all-one-list.pl
+++
@@ -1,5 +0,0 @@
-#insert x into a list in all possible positions in one list.
-
-insert(X, [], [X]).
-insert(X, [Y|T], [X,Y|T]).
-insert(X, [Y|T], [Y|Z]) :- insert(X, T, Z).
--- Practical/add-list-all.pl
+++
@@ -1,5 +0,0 @@
-#insert x into a list in all possible positions.(use ; in the output to continue)
-
-ins(X,[],[X]).
-ins(X,[H|T],[X,H|T]).
-ins(X,[H|T],[H|L]):-ins(X,T,L).
